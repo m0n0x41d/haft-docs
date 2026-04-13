c616b168bab4dda40e27e191a3dfb8a063eb837c
Ivan Zakutnii <zakutnii.ivan@gmail.com>
2026-04-14 01:13:19 +0400

    fix script

--- public/install.sh
+++ public/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# quint-code installer wrapper
+# haft installer wrapper
 # Fetches and runs the latest install script from GitHub
 
-curl -fsSL https://raw.githubusercontent.com/m0n0x41d/quint-code/main/install.sh | bash -s -- "$@"
+curl -fsSL https://raw.githubusercontent.com/m0n0x41d/haft/main/install.sh | bash -s -- "$@"
